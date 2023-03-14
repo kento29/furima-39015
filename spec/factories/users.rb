fcf683a3ed5d1e16fecab18313a92435f394a5cb
k-mstn <knt.masutani@gmail.com>
2023-03-14 14:47:55 +0900

    FactoryBot

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,16 +1,16 @@
-# FactoryBot.define do
-#   factory :user do
-#     nickname              {'test'}
-#     email                 {'test@example'}
-#     password              {'000000'}
-#     password_confirmation {password}
-#     last_name             {原}
-#     first_name            {ゆたか}
-#     last_name_kana        {ハラ}
-#     first_name_kana       {ユタカ}
-
-#   end
-# end
+FactoryBot.define do
+  factory :user do
+    nickname              {'test'}
+    email                 {'test@example'}
+    password              {'abc000'}
+    password_confirmation {password}
+    last_name             {'原'}
+    first_name            {'ゆたか'}
+    last_name_kana        {'ハラ'}
+    first_name_kana       {'ユタカ'}
+    user_birth_date       {'2000-01-01'}
+  end
+end
 
 # FactoryBot.define do
 #   factory :user do
