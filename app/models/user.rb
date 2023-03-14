c4079779000716e128bdeea9ced824e73f0b5de0
k-mstn <knt.masutani@gmail.com>
2023-03-14 16:55:09 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,6 @@ class User < ApplicationRecord
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
   validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "Full-width characters"}
   validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "Full-width characters"}
-  validates :last_name_kana, format: {with: /\A[ァ-ヶ一-]+\z/, message: "Last_name_kana Full-width katakana characters"}
-  validates :first_name_kana, format: {with: /\A[ァ-ヶ一-]+\z/, message: "First_name_kana kana Full-width katakana characters"}
+  validates :last_name_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "Last_name_kana Full-width katakana characters"}
+  validates :first_name_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "First_name_kana kana Full-width katakana characters"}
 end
