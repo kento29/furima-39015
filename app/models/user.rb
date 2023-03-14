9a67b5df6d44fa4f5d0bd42bc645aa1b1f75d541
k-mstn <knt.masutani@gmail.com>
2023-03-14 14:48:04 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
+  validates :nickname,        presence: true
+  validates :last_name,       presence: true
+  validates :first_name,      presence: true
+  validates :last_name_kana,  presence: true
   validates :first_name_kana, presence: true
+  validates :user_birth_date, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+  validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "Full-width characters"}
+  validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "Full-width characters"}
+  validates :last_name_kana, format: {with: /\A[ァ-ヶ]+\z/, message: "Last_name_kana Full-width katakana characters"}
+  validates :first_name_kana, format: {with: /\A[ァ-ヶ]+\z/, message: "First_name_kana kana Full-width katakana characters"}
+
+
 end
