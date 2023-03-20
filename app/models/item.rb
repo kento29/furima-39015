40d0b0ded4fbb78a77dcc3cc390abfe91d3953c8
k-mstn <knt.masutani@gmail.com>
2023-03-20 15:19:35 +0900

    Rubocop

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,6 +15,12 @@ class Item < ApplicationRecord
   validates :shipping_fee_status_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :scheduled_delivery_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "は、¥300~¥9,999,999の間のみ保存可能" }
-  validates :price, numericality: { only_integer: true, message: "は半角数値のみ保存可能" }
+  validates :price,
+            numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
+                            message: 'は、¥300~¥9,999,999の間のみ保存可能' }
+  validates :price, numericality: { only_integer: true, message: 'は半角数値のみ保存可能' }
+
+  # def sold_out?
+  #   self.stock <= 0
+  # end
 end
