1d2d860f1f3f9b0e751cca5b6c1a08a10055780b
k-mstn <knt.masutani@gmail.com>
2023-03-21 19:54:38 +0900

    destroyアクションの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -36,9 +36,10 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def destroy
-  #   @item = Item.find(params[:id])
-  # end
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
 
   private
 
